98cd91cd6e29581d484d74fd77da6343f1fb5863
TAKUGABRIEL <takugabriel.tg@gmail.com>
2023-04-18 00:50:59 +0000

    fix

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,57 +2,68 @@ import random
 
 my_score = 0
 cpu_score = 0
-select = input("Rock Paper Scissors: ").lower()
 
-while select != "rock" and select != "paper" and select != "scissors":
-    print("Invalid. Try again!")
-    select = input("Rock Paper Scissors: ")    
-    if select == "rock" or select == "paper" or select == "scissors":
+while True:
+    try:
+        score = int(input("Enter the score you want to reach?: "))
         break
+    except ValueError:
+        "Enter a number"
 
-while my_score < 10 and cpu_score < 10:
-    number = random.randint(1, 3)    
+while my_score < score and cpu_score < score:
+    number = random.randint(1, 3)
+
+    select = input("Rock Paper Scissors: ").lower()
+
+    if select == "quit":
+        while True:
+            quit_game = input("Do you want to quit? Enter Y or N: ").lower()
+            if quit_game == "y":
+                print("See you later!")
+                quit()
+            elif quit_game == "n":
+                break
+            
     if select == "rock":
         if number == 1:
-            print("Rock Rock. Draw game!")
-            print(f"my score: {my_score} cpu score: {cpu_score}")
+            print("Rock Rock!")
+            print(f"{my_score} - {cpu_score}")
         if number == 2:
-            print("Paper covers Rock. You lost!")
+            print("Paper covers Rock!")
             cpu_score += 1
-            print(f"my score: {my_score} cpu score: {cpu_score}")
+            print(f"{my_score} - {cpu_score}")
         if number == 3:
-            print("Rock breaks scissors. You won!")
+            print("Rock breaks scissors!")
             my_score += 1
-            print(f"my score: {my_score} cpu score: {cpu_score}")
+            print(f"{my_score} - {cpu_score}")
     elif select == "paper":
         if number == 1:
-            print("Paper covers Rock. You won!")
+            print("Paper covers Rock!")
             my_score += 1
-            print(f"my score: {my_score} cpu score: {cpu_score}")
+            print(f"{my_score} - {cpu_score}")
         if number == 2:
-            print("Paper Paper. Draw game!")
-            print(f"my score: {my_score} cpu score: {cpu_score}")
+            print("Paper Paper!")
+            print(f"{my_score} - {cpu_score}")
         if number == 3:
-            print("Scissors cuts paper. You lost!")
+            print("Scissors cuts paper!")
             cpu_score += 1
-            print(f"my score: {my_score} cpu score: {cpu_score}")
+            print(f"{my_score} - {cpu_score}")
     elif select == "scissors":
         if number == 1:
-            print("Rock breaks scissors. You lost!")
+            print("Rock breaks scissors!")
             cpu_score += 1
-            print(f"my score: {my_score} cpu score: {cpu_score}")
+            print(f"{my_score} - {cpu_score}")
         if number == 2:
-            print("Scissors cuts paper. You won!")
+            print("Scissors cuts paper!")
             my_score += 1
-            print(f"my score: {my_score} cpu score: {cpu_score}")
+            print(f"{my_score} - {cpu_score}")
         if number == 3:
-            print("Scissors Scissors. Draw game!")
-            print(f"my score: {my_score} cpu score: {cpu_score}")
-    select = input("Rock Paper Scissors: ")
+            print("Scissors Scissors!")
+            print(f"{my_score} - {cpu_score}")
+    
 
 print(f"My Score : {my_score}  Computer Score : {cpu_score}")
 if my_score > cpu_score:
     print("Congratulations. You won!")
 else:
     print("Sorry. You lost")
-
